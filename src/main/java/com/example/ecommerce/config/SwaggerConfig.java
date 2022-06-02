4a38aa7f4c413787efc693d7bceca917280b4c44
Andrey Fabricio <andrey.fas@gmail.com>
2022-06-02 17:56:15 -0300

    Update SwaggerConfig.java

--- src/main/java/com/example/ecommerce/config/SwaggerConfig.java
+++ src/main/java/com/example/ecommerce/config/SwaggerConfig.java
@@ -18,15 +18,11 @@ public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
-//                .apiInfo(getApiInfo())
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.example.ecommerce"))
-//                .paths(PathSelectors.any())
-//                .build();
-        		.select()
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.ecommerce"))
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(getApiInfo())
                 .useDefaultResponseMessages(false);
     }
     
